c9bd1d46ce4b95ccb0f5db12a48a8c517b890f84
U-KAMINARIO\yogev.vaknin <yogev.vaknin@KMC165.kaminario.local>
2015-09-14 18:27:14 +0300

    Fix line offset on __isForLoop and __containBracket
    the functions count from zero, but we count from line number 1
    
    Signed-off-by: U-KAMINARIO\yogev.vaknin <yogev.vaknin@KMC165.kaminario.local>

--- autoPar/injectPlanner.py
+++ autoPar/injectPlanner.py
@@ -23,7 +23,7 @@ class InjectPlanner(object):
     def __isForLoop(self,fileName,lineNumber):
         f=open(fileName)
         lines=f.readlines()
-        if "for" in lines[lineNumber]:
+        if "for" in lines[lineNumber-1]:
             return True
         else:
             return False
@@ -32,7 +32,7 @@ class InjectPlanner(object):
     def __containBracket(self,fileName,lineNumber):
         f=open(fileName)
         lines=f.readlines()
-        if "}" in lines[lineNumber]:
+        if "}" in lines[lineNumber-1]:
             return True
         else:
             return False
