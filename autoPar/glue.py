dba85506fd42670009afb47aedbfe0b27c4b4a0d
U-KAMINARIO\yogev.vaknin <yogev.vaknin@KMC165.kaminario.local>
2015-09-21 15:03:01 +0300

    Change bitcode genration command
    Signed-off-by: U-KAMINARIO\yogev.vaknin <yogev.vaknin@KMC165.kaminario.local>

--- autoPar/glue.py
+++ autoPar/glue.py
@@ -5,6 +5,7 @@ import logging
 
 '''
 clang -O0 -c -emit-llvm ./src/Samples/sample1.c -o ./src/Samples/sample1.cb
+clang-3.6 -cc1 ${CUR_FILE} -I/usr/include/clang/3.6/include -I/usr/include/`arch`-linux-gnu -I/usr/include/ -v -emit-llvm -g -O0 -o ${BITCODE_FILE}
 opt-3.6 -load ../src/LLVMPass/libIndependentLoop.so -basicaa -mem2reg -simplifycfg -loop-simplify -loop-rotate -instcombine -indvars -indloop ./src/Samples/sample1.bc -o /dev/null 2> ./autoPar/raw_input.txt
 '''
 
@@ -28,7 +29,7 @@ class Gluer(object):
     def genBitcodeForAllFiles(self, files):
         for file in files:
             bitcodeFilename =file[:-1] + "bc"
-            commandString = "clang-3.6 -O0 -c -emit-llvm " +file +" -o " + bitcodeFilename
+            commandString = "clang-3.6 -cc1 " +file +"  -I/usr/include/clang/3.6/include -I/usr/include/`arch`-linux-gnu -I/usr/include/ -v -emit-llvm -g -O0 -o " + bitcodeFilename
             try:
                 os.system(commandString)
             except:
@@ -38,7 +39,7 @@ class Gluer(object):
     def genAllOutput(self,files):
         for file in files:
             bitcodeFilename =file[:-1] + "bc"
-            commandString = "opt-3.6 -load ../src/LLVMPass/libIndependentLoop.so -basicaa -mem2reg -simplifycfg -loop-simplify -loop-rotate -instcombine -indvars -indloop "+bitcodeFilename + "  -o /dev/null 2>> ./raw_input.txt"
+            commandString = "opt-3.6 -load ../src/LLVMPass/libIndependentLoop.so -basicaa -mem2reg -simplifycfg -loop-simplify -loop-rotate -instcombine -indvars -indloop "+bitcodeFilename + "  -o /dev/null &>> ./raw_input.txt"
             try:
                 logging.info('Running %s', bitcodeFilename)
                 os.system(commandString)
